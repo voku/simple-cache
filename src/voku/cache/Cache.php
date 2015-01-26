1aa284f9aadaa945553f1072fde9f514670ddfad
Lars Moelleken <lmoelleken@menadwork.com>
2015-01-26 15:24:41 +0100

    [!!!]: added a parameter to disable the cache

--- src/voku/cache/Cache.php
+++ src/voku/cache/Cache.php
@@ -47,24 +47,39 @@ class Cache implements iCache
    *
    * @param iAdapter    $adapter
    * @param iSerializer $serializer
-   * @param boolean     $checkForUser | check for dev-ip or if cms user is logged-in ...
+   * @param boolean     $checkForUser check for dev-ip or if cms user is logged-in ...
+   * @param bool        $cacheEnabled false will disable the cache (use it e.g. for global settings)
    */
-  public function __construct($adapter = null, $serializer = null, $checkForUser = true)
+  public function __construct($adapter = null, $serializer = null, $checkForUser = true, $cacheEnabled = true)
   {
     static $adapterCache;
+
+    // check for active-cache
+    $this->setActive($cacheEnabled);
+    if ($this->active !== true) {
+      return false;
+    }
 
     // test the cache also for dev
     $testCache = isset($_GET['testCache']) ? (int) $_GET['testCache'] : 0;
 
-    // check for user-session / dev / ip / config && no testCache is set
+    // check for user-session / dev / ip && no testCache is set
     if ($checkForUser === true && $testCache != 1) {
       if (
-        // cache isn't active
-          $this->active !== true ||
           // $_SERVER == client
-          $_SERVER['SERVER_ADDR'] == $this->get_client_ip() ||
+          (
+              isset($_SERVER['SERVER_ADDR'])
+              &&
+              $_SERVER['SERVER_ADDR'] == $this->get_client_ip()
+          )
+          ||
           // user is loggedIn
-          $_SESSION['userID'] ||
+          (
+              isset($_SESSION['userID'])
+              &&
+              $_SESSION['userID']
+          )
+          ||
           // user is a dev
           $this->checkForDev() === true
       ) {
@@ -439,7 +454,5 @@ class Cache implements iCache
   {
     $this->active = boolval($active);
   }
-
-
 
 }
